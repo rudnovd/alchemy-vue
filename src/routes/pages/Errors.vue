b7c2355fa2e72bcf72c508581103904a4e721e2a
Hyuugo <rudnov.d@gmail.com>
2019-02-20 14:36:02 +0500

    edit text on home, errors pages

--- src/routes/pages/Errors.vue
+++ src/routes/pages/Errors.vue
@@ -2,7 +2,9 @@
   b-container
     b-row: b-col(cols='12')
       .jumbotron
-          h1 Страница не найдена!
+          h1 Page not found (404)
+          p You may have entered the wrong address or the page was deleted.
+          p Back to the main page
 </template>
 
 <script>
--- src/routes/pages/Home.vue
+++ src/routes/pages/Home.vue
@@ -2,9 +2,9 @@
   b-container
     b-row: b-col(cols='12')
       .jumbotron
-          h1 Алхимия - логическая онлайн игра
+          h1 Alchemy — logical online game.
           p.lead
-            | Весь наш мир состоит из огромного количества элементов, а они же в свою очередь, в той или иной степени, состоят из четырёх основных элементов: огня, воды, земли и воды. В нашей игре мы даём тебе возможность воссоздать весь процесс развития Земли, зарождая свой собственный мир!
+            | Our whole world consists of a huge number of elements. In our game, we give you the opportunity to recreate the entire process of the development of the Earth, creating your own world! You can combine elements, thereby creating more and more complex substances. No one will say exactly how to combine the resulting things right! Use your logic and ingenuity to make progress, but if you fail to discover something new, you can always rely on luck. We dare not hold you any longer, go ahead!
 </template>
 
 <script>
