80394eb960618c7e951a21e48ac16fff90e0946e
Hyuugo <rudnov.d@gmail.com>
2019-04-14 23:03:12 +0500

    Game data now stored in global store

--- src/routes/pages/Game.vue
+++ src/routes/pages/Game.vue
@@ -4,59 +4,104 @@
       vue-draggable-resizable(
         class-name-active='active-element'
         class-name='element'
-        v-for='(value, index) in data.elements'
-        :key='value'
+        v-for='(value, index) in openedElements'
+        :key='value._id'
         :resizable='false'
         :w='100'
         :h='50'
-        :x='data.elementsX[index]'
-        :y='data.elementsY[index]'
+        :x='activeElements[index].x'
+        :y='activeElements[index].y'
         :parent='true'
-        :on-drag-start='onDragStartCallback'
         @dragging='onDrag'
-      ) {{ value }}
+        @activated="onActivated(value)"
+        @dragstop="onDragstop(value)"
+      ) {{ value.name }}
       b-btn.clear-field-button(variant='link' @dblclick='clearGameField')
         font-awesome-icon.fa-3x(icon='trash')
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
   name: 'Game',
   mounted () {
-    const playerScreen = document.getElementsByClassName('game')
-    this.data.playerScreenWidth = playerScreen.clientWidth
-    this.data.playerScreenHeight = playerScreen.clientHeight
+    const gameField = document.getElementsByClassName('game-field')
+    this.setGameFieldSize({ x: gameField[0].clientWidth, y: gameField[0].clientHeight })
+
     window.addEventListener('resize', () => {
-      this.data.playerScreenWidth = playerScreen.clientWidth
-      this.data.playerScreenHeight = playerScreen.clientHeight
+      this.setGameFieldSize({ x: gameField[0].clientWidth, y: gameField[0].clientHeight })
+    })
+
+    this.setOpenedElements([
+      {
+        _id: '5c543755405e9d103878d4be',
+        name: 'Earth',
+        category: 'Elements'
+      },
+      {
+        _id: '5c543755405e9d103878d4bf',
+        name: 'Fire',
+        category: 'Elements'
+      },
+      {
+        _id: '5c543755405e9d103878d4c0',
+        name: 'Water',
+        category: 'Elements'
+      }
+    ])
+
+    this.setActiveElements([
+      {
+        _id: '5c543755405e9d103878d4be',
+        name: 'Earth',
+        category: 'Elements',
+        x: 200,
+        y: 250
+      },
+      {
+        _id: '5c543755405e9d103878d4bf',
+        name: 'Fire',
+        category: 'Elements',
+        x: 400,
+        y: 250
+      },
+      {
+        _id: '5c543755405e9d103878d4c0',
+        name: 'Water',
+        category: 'Elements',
+        x: 600,
+        y: 250
+      }
+    ])
+  },
+  computed: {
+    ...mapGetters({
+      getGameFieldSize: 'game/getGameFieldSize',
+      openedElements: 'game/openedElements',
+      activeElements: 'game/activeElements',
+      selectedElement: 'game/selectedElement'
     })
   },
   data () {
     return {
-      data: {
-        playerScreenWidth: 0,
-        playerScreenHeight: 0,
-
-        activeElement: {
-          x: null,
-          y: null,
-          z: null
-        },
-
-        elements: ['Earth', 'Water', 'Fire'],
-
-        elementsX: [400, 600, 800],
-        elementsY: [250, 250, 250]
-      }
     }
   },
   methods: {
+    ...mapActions({
+      setGameFieldSize: 'game/setGameFieldSize',
+      setOpenedElements: 'game/setOpenedElements',
+      setActiveElements: 'game/setActiveElements',
+      setSelectedElement: 'game/setSelectedElement'
+    }),
     onDrag (x, y) {
       console.log(`X: ${x}, Y: ${y}`)
     },
-    onDragStartCallback (ev) {
-      const activeElement = document.getElementsByClassName('active-element')
-      console.log(activeElement)
+    onActivated (element) {
+      this.setSelectedElement(element)
+    },
+    onDragstop (element) {
+      this.setSelectedElement(element)
     },
     clearGameField () {
       console.log('clearGameField method called')
@@ -76,6 +121,7 @@ export default {
   right: 10vw;
   box-shadow: 0 0 5px $alchemy-light-green;
   background-color: white;
+  min-width: 80vw;
 }
 
 .element {
