ea183dc851f6ec674a2e0c486f851d1cef07f43f
Hyuugo <rudnov.d@gmail.com>
2019-04-18 12:20:29 +0500

    Update global store: adding, removing elements on game field, updating selected element coordinates

--- src/store/modules/game/actions.js
+++ src/store/modules/game/actions.js
@@ -2,11 +2,22 @@ export default {
   setOpenedElements ({ commit }, elements) {
     commit('SET_OPENED_ELEMENTS', elements)
   },
+
   setActiveElements ({ commit }, elements) {
     commit('SET_ACTIVE_ELEMENTS', elements)
   },
+  addActiveElement ({ commit }, element) {
+    commit('ADD_ACTIVE_ELEMENT', element)
+  },
+  removeActiveElement ({ commit }, elements) {
+    commit('REMOVE_ACTIVE_ELEMENT', elements)
+  },
+
   setSelectedElement ({ commit }, element) {
     commit('SET_SELECTED_ELEMENT', element)
+  },
+  setSelectedElementCoordinates ({ commit }, { x, y }) {
+    commit('SET_SELECTED_ELEMENT_COORDINATES', { x, y })
   },
 
   setGameFieldSize ({ commit }, { x, y }) {
--- src/store/modules/game/mutations.js
+++ src/store/modules/game/mutations.js
@@ -2,15 +2,31 @@ export default {
   SET_OPENED_ELEMENTS (state, elements) {
     state.openedElements = elements
   },
+
   SET_ACTIVE_ELEMENTS (state, elements) {
     state.activeElements = elements
   },
+  ADD_ACTIVE_ELEMENT (state, element) {
+    element.x = state.selectedElement.x
+    element.y = state.selectedElement.y
+
+    state.activeElements.push(element)
+  },
+  REMOVE_ACTIVE_ELEMENT (state, elements) {
+    for (let i = 0; i < elements.length; i++) {
+      state.activeElements.splice(state.activeElements.indexOf(elements[i]), 1)
+    }
+  },
+
   SET_SELECTED_ELEMENT (state, element) {
     state.selectedElement = element
   },
+  SET_SELECTED_ELEMENT_COORDINATES (state, { x, y }) {
+    state.selectedElement.x = x
+    state.selectedElement.y = y
+  },
 
   SET_GAME_FIELD_SIZE (state, { x, y }) {
-    console.log(x, y)
     state.settings.gameFieldSize.x = x
     state.settings.gameFieldSize.y = y
   }
