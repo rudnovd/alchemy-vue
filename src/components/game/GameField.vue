e0ef13cebe01362e7ea905a5c96bf1ad0f073170
rudnovd <rudnov.d@gmail.com>
2019-07-29 18:06:01 +0500

    updated game field component css

--- src/components/game/GameField.vue
+++ src/components/game/GameField.vue
@@ -1,5 +1,5 @@
 <template lang='pug'>
-b-col(class='game-field' cols='12')
+b-col(class='game-field pl-0 pr-0' cols='12')
   slot
 </template>
 
@@ -9,11 +9,13 @@ b-col(class='game-field' cols='12')
 
 <style lang='scss' scoped>
 .game-field {
-  position: relative;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
   box-shadow: 0 0 5px color('alchemy-light-green');
   background-color: color('white');
-  min-height: 60vh;
-  min-width: 100%;
-  max-width: inherit;
+  min-height: 85vh;
 }
 </style>
