6541488cc3c3298a82becfa8b451374652397c67
rudnovd <rudnov.d@gmail.com>
2019-08-05 03:15:35 +0500

    removed opened recipes list, added opened recipes modal

--- src/components/game/OpenedRecipe.vue
+++ src/components/game/OpenedRecipe.vue
@@ -1,29 +1,17 @@
 <template lang='pug'>
-b-btn(
-  class='mr-2'
-  :id='`popover-${ recipeName }`'
-  variant='info'
-) {{ recipeName }}
-  b-popover(
-    :target='`popover-${ recipeName }`'
-    :show.sync='showPopover'
-    triggers='hover'
-    placement='top'
-    :content='recipeName'
-  )
+b-col(class='opened-recipe-col' cols='12')
+  b-row(class='row align-items-center')
+    b-col(cols='6')
+      p {{ recipe.result.name }}
+      p descrption
+    b-col(class="text-center" cols='6')
+      | {{ recipe.recipe[0].name }} + {{ recipe.recipe[1].name }}
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-
 export default {
   props: {
-    recipeName: String
-  },
-  computed: {
-    ...mapGetters({
-      openedRecipes: 'game/selectedCategory'
-    })
+    recipe: Object
   },
   data () {
     return {
@@ -34,5 +22,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.opened-recipe-col {
+  background-color: rgb(240, 240, 240);
+  display: inline-block;
+  border: 1px solid black;
+  border-radius: 10px;
+  margin-bottom: 20px;
+}
 </style>
---
+++ src/components/game/OpenedRecipesModal.vue
@@ -0,0 +1,74 @@
+<template lang='pug'>
+b-modal(
+  v-model='showModal'
+  size='lg'
+  hide-header=true
+  hide-footer=true
+  centered=true
+)
+  b-row(class='ml-3 mr-3')
+    //- text
+    b-col(class='mt-4' cols='8')
+      h4 Opened recipes
+
+    //- Close button
+    b-col(class='ml-auto text-right' cols='2')
+      b-button(
+        class='close-button'
+        size='sm'
+        variant='link'
+        @click='showModal = false'
+      )
+        font-awesome-icon(class='c-pointer fa-2x' icon='times')
+
+    b-col(class='mt-2' cols='4')
+      b-btn(
+        block
+        v-for='category in openedCategories'
+        :key='category._id'
+        :class='{ "btn-success": category.name === selectedCategory}'
+        @click='selectedCategory = category.name'
+      ) {{ category.name }}
+
+    b-col(class='mt-2' cols='8')
+      OpenedRecipe(
+        v-for='recipe in openedRecipes'
+        :key='recipe._id'
+        :recipe='recipe'
+      )
+
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+import OpenedRecipe from '@/components/game/OpenedRecipe.vue'
+
+export default {
+  components: {
+    OpenedRecipe
+  },
+  data () {
+    return {
+      showModal: false,
+      selectedCategory: 'Elements'
+    }
+  },
+  mounted () {
+    this.$root.$on('openedRecipesModalShow', () => {
+      this.showModal = true
+    })
+  },
+  computed: {
+    ...mapGetters({
+      openedCategories: 'game/openedCategories',
+      openedRecipes: 'game/openedRecipes'
+    })
+  },
+  methods: {
+    ...mapActions({
+      setUser: 'user/setUser'
+    })
+  }
+}
+</script>
