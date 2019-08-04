c16c3500cc3f7cbe9b680798df5378733cb67fd9
rudnovd <rudnov.d@gmail.com>
2019-08-04 12:53:43 +0500

    added component for view info about opened recipes

---
+++ src/components/game/OpenedRecipe.vue
@@ -0,0 +1,38 @@
+<template lang='pug'>
+b-btn(
+  class='mr-2'
+  :id='`popover-${ recipeName }`'
+  variant='info'
+) {{ recipeName }}
+  b-popover(
+    :target='`popover-${ recipeName }`'
+    :show.sync='showPopover'
+    triggers='hover'
+    placement='top'
+    :content='recipeName'
+  )
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  props: {
+    recipeName: String
+  },
+  computed: {
+    ...mapGetters({
+      openedRecipes: 'game/selectedCategory'
+    })
+  },
+  data () {
+    return {
+      showPopover: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/game/OpenedRecipesList.vue
@@ -0,0 +1,12 @@
+<template lang='pug'>
+b-col(class='opened-recipes pl-0 mt-3' cols='7' offset='2')
+  slot
+</template>
+
+<script>
+
+</script>
+
+<style lang='scss' scoped>
+
+</style>
