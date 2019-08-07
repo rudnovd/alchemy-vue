0f0b86b2720f2e6df507657745fead4b3f672d46
rudnovd <rudnov.d@gmail.com>
2019-08-08 02:58:49 +0500

    updated clear game field button: added help popover, clear game field by doubleclick, updated styles

--- src/components/game/ClearGameField.vue
+++ src/components/game/ClearGameField.vue
@@ -1,21 +1,61 @@
 <template lang='pug'>
-font-awesome-icon(class='fa-3x clear-game-field-button' icon='trash' @dblclick='removeAllActiveElements')
+div(class='clear-game-field-button')
+  font-awesome-icon(id='clear-game-field-button' class='clear-game-field-button' icon='trash' @click='onClick')
+  b-popover(
+    target='clear-game-field-button'
+    placement='bottom'
+    content='Click for clear game field'
+    :show='showPopover'
+    @shown='onShown'
+  )
 </template>
 
 <script>
 import { mapActions } from 'vuex'
 
 export default {
+  data () {
+    return {
+      showPopover: false,
+      isTimeout: false
+    }
+  },
   methods: {
     ...mapActions({
       removeAllActiveElements: 'game/removeAllActiveElements'
-    })
+    }),
+    onClick () {
+      if (!this.showPopover && !this.isTimeout) {
+        this.showPopover = true
+      } else if (this.showPopover && this.isTimeout) {
+        setTimeout(() => {
+          this.isTimeout = false
+          this.showPopover = false
+          this.removeAllActiveElements()
+        }, 0)
+      }
+    },
+    onShown () {
+      if (!this.isTimeout) {
+        this.isTimeout = true
+        setTimeout(() => {
+          this.isTimeout = false
+          this.showPopover = false
+        }, 1500)
+      }
+    }
   }
 }
 </script>
 
-<style lang='scss'>
+<style lang='scss' scoped>
 .clear-game-field-button {
+  cursor: pointer;
   color: color('dark');
+  order: 0;
+  margin-left: auto;
+  width: 30px;
+  height: 30px;
+  padding: 0;
 }
 </style>
