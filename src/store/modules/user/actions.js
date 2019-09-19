25658351cb7f60b5a5917c10272076e39fcf474e
rudnovd <rudnov.d@gmail.com>
2019-09-20 02:12:42 +0500

    updated user actions in global store

--- src/store/modules/user/actions.js
+++ src/store/modules/user/actions.js
@@ -1,14 +1,16 @@
 import { getLogin, getLogout } from '@/js/api/authentication'
 
 export default {
-  async getLogin ({ commit }) {
-    commit('LOADING_START')
-    await getLogin().then(response => {
-      commit('LOADING_END')
-      if (response.status === 200) {
-        commit('SET_USER', response.data.user)
-      }
-    })
+  async getLogin ({ state, commit }) {
+    if (!state.isLoggedIn) {
+      commit('LOADING_START')
+      await getLogin().then(response => {
+        commit('LOADING_END')
+        if (response.status === 200) {
+          commit('SET_USER', response.data.user)
+        }
+      })
+    }
   },
   async getLogout ({ commit }) {
     commit('LOADING_START')
@@ -18,5 +20,8 @@ export default {
         commit('DELETE_USER')
       }
     })
+  },
+  setUser ({ commit }, user) {
+    commit('SET_USER', user)
   }
 }
