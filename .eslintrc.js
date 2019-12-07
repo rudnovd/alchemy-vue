d4a1ec8f0fa374cbd0ec0ec0e0aea3792658515f
rudnovd <rudnov.d@gmail.com>
2019-12-07 14:46:10 +0500

    feat(eslint): extends new style rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+
+  extends: ['plugin:vue/essential', 'plugin:vue/strongly-recommended', 'plugin:vue/recommended', '@vue/prettier'],
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
+
   parserOptions: {
     parser: 'babel-eslint'
   }
