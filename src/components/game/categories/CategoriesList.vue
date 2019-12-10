f2dfe125d75d733476770fdd726372dcc73625b4
rudnovd <rudnov.d@gmail.com>
2019-12-10 19:30:41 +0500

    refactor(CategoriesList component): update styles

--- src/components/game/categories/CategoriesList.vue
+++ src/components/game/categories/CategoriesList.vue
@@ -14,18 +14,19 @@
       <button
         v-for="openedCategory in openedCategories"
         :key="openedCategory._id"
-        class="select-category-button"
+        class="list-button"
         :class="{ active: openedCategory._id === selectedCategory._id }"
         @click="selectCategory(openedCategory)"
       >
         <b-img
+          class="list-button-image"
           :src="`/images/categories/${openedCategory.name}.png`"
           width="45"
           height="45"
           :alt="openedCategory.name"
           @error="setBaseIcon"
         />
-        <span>{{ openedCategory.name }}</span>
+        <span class="list-button-name">{{ openedCategory.name }}</span>
       </button>
     </template>
   </div>
@@ -67,116 +68,115 @@ export default {
 <style lang="scss" scoped>
 .categories-list {
   display: flex;
+  flex-wrap: wrap;
+  flex: 0 0 100%;
   justify-content: flex-start;
-  flex: 1 0 0;
-  overflow-y: auto;
   overflow-x: hidden;
-  max-height: 90vh;
-
+  overflow-y: auto;
+  padding: 5px;
   @extend %scrollbar;
 
-  .loading,
-  .error {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
-    width: 100%;
-
-    .spinner {
-      color: map-get($colors, 'alchemy-green');
-      width: 3em;
-      height: 3em;
-    }
-  }
-
-  .select-category-button {
-    display: flex;
-    align-content: center;
-    align-items: center;
-    justify-content: center;
-    font-size: 16px;
-    text-align: center;
-    opacity: 0.5;
-    word-break: break-word;
-    background: rgb(240, 240, 240);
-    border: 1px solid map-get($colors, 'alchemy-light-green');
-    border-radius: 6px;
-    transition: background-color 0.4s;
-
-    &.active {
-      font-weight: bold;
-      opacity: 1;
-      color: rgb(255, 255, 255);
-      background-color: map-get($colors, 'alchemy-green');
-      border: 1px solid map-get($colors, 'alchemy-light-green');
-    }
-
-    img {
-      margin-right: 5px;
-    }
-  }
-}
-
-@media screen and (min-width: map-get($grid-breakpoints, 'md')) {
-  .categories-list {
-    flex-direction: column;
-    align-items: center;
-    align-content: center;
-    direction: rtl;
-    padding-left: 10px;
-    padding-right: 10px;
-
-    .select-category-button {
-      height: 70px;
-      width: 100%;
-      font-size: 1.2em;
-      margin-bottom: 12px;
-
-      &:hover {
-        opacity: 0.8;
-        border: 1px solid map-get($colors, 'alchemy-dark-green');
-      }
-
-      img {
-        height: 45px;
-        width: 45px;
-        order: 1;
-      }
-    }
-  }
-}
-
-@media screen and (max-width: map-get($grid-breakpoints, 'md')) {
-  .categories-list {
+  @include media-sm {
     height: 100%;
     padding: 5px;
-    flex-wrap: wrap;
+  }
 
-    .select-category-button {
-      flex: 0 0 32%;
-      height: 50px;
-      font-size: 1em;
-      margin-right: 5px;
-      margin-bottom: 5px;
+  @include media-lg {
+    align-content: center;
+    align-items: center;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    max-height: 90vh;
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+}
 
-      img {
-        height: 25px;
-        width: 25px;
-      }
+.list-button {
+  align-content: center;
+  align-items: center;
+  background: rgb(240, 240, 240);
+  border-radius: 6px;
+  border: 1px solid map-get($colors, 'light-green');
+  display: flex;
+  flex-wrap: nowrap;
+  flex: 0 0 32%;
+  font-size: 0.8rem;
+  height: 2.5rem;
+  justify-content: center;
+  margin-bottom: 0.5rem;
+  opacity: 0.5;
+  text-align: center;
+  transition: background-color 0.4s;
+  word-break: break-word;
+  flex-direction: column;
+
+  &.active {
+    font-weight: bold;
+    opacity: 1;
+    color: rgb(255, 255, 255);
+    background-color: map-get($colors, 'green');
+    border: 1px solid map-get($colors, 'light-green');
+  }
+
+  @include media-sm {
+    flex: 0 0 31.625%;
+    height: 3rem;
+    margin-left: 5px;
+    min-width: 90px;
+  }
+
+  @include media-md {
+    flex-direction: row;
+    flex: 0 0 24%;
+  }
+
+  @include media-lg {
+    flex: 0 0 5rem;
+    font-size: 1.2em;
+    height: 70px;
+    margin-bottom: 12px;
+    width: 100%;
+
+    &:hover {
+      border: 1px solid map-get($colors, 'dark-green');
+      opacity: 0.8;
     }
   }
 }
 
-@media screen and (max-width: map-get($grid-breakpoints, 'min')) {
-  .categories-list {
-    .select-category-button {
-      font-size: 0.8rem;
+.list-button-image {
+  display: none;
+  flex: 0 0 auto;
+  order: 0;
 
-      img {
-        display: none;
-      }
-    }
+  @include media-sm {
+    display: flex;
+    margin-right: 0.2rem;
+    max-height: 1.2rem;
+    max-width: 1.2rem;
+  }
+
+  @include media-md {
+    display: flex;
+    margin-right: 5px;
+    max-height: 23px;
+    max-width: 23px;
+  }
+
+  @include media-lg {
+    max-height: 45px;
+    max-width: 45px;
+  }
+}
+
+.list-button-name {
+  display: flex;
+  justify-content: flex-start;
+  flex: 0 0 auto;
+
+  @include media-md {
+    flex: 0 0 80%;
   }
 }
 </style>
