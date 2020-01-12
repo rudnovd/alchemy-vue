a9b101ae2cf641cec057ea610bea0acff3490d12
rudnovd <rudnov.d@gmail.com>
2020-01-13 01:56:47 +0500

    refactor(OpenedRecipesModal): refactor component functions

--- src/components/game/recipes/OpenedRecipesModal.vue
+++ src/components/game/recipes/OpenedRecipesModal.vue
@@ -1,31 +1,38 @@
 <template>
-  <b-modal v-model="showModal" size="xl" hide-header="hide-header" hide-footer="hide-footer" centered="centered">
-    <b-row class="ml-1 mr-1">
+  <b-modal
+    :visible="showModal"
+    size="xl"
+    hide-header="hide-header"
+    hide-footer="hide-footer"
+    centered="centered"
+    @hide="onClose"
+  >
+    <b-row class="ml-1 mr-1 mb-3">
       <b-col cols="9">
         <h4>Opened recipes</h4>
       </b-col>
 
       <b-col class="ml-auto text-right" cols="3">
-        <button class="close-button" @click="showModal = false">
+        <button class="close-button" @click="onClose">
           <font-awesome-icon icon="times" />
         </button>
       </b-col>
     </b-row>
 
     <b-row class="recipes-row">
-      <b-col class="mt-2 opened-recipes-categories" cols="12" sm="12" md="12" lg="4" xl="4">
+      <b-col class="mt-2 opened-recipes-categories" cols="12" lg="3">
         <button
           v-for="category in openedCategories"
           :key="category._id"
           class="category"
           :class="{ active: category._id === selectedCategory._id }"
-          @click="selectCategory(category)"
+          @click="selectedCategory = category"
         >
           {{ category.name }}
         </button>
       </b-col>
 
-      <b-col class="mt-2 recipes-list" cols="12" sm="12" md="12" lg="8" xl="8">
+      <b-col class="mt-2 recipes-list" cols="12" lg="8">
         <OpenedRecipe v-for="recipe in filteredByCategoryRecipes" :key="recipe._id" :recipe="recipe" />
       </b-col>
     </b-row>
@@ -42,9 +49,15 @@ export default {
   components: {
     OpenedRecipe
   },
+  props: {
+    showModal: {
+      type: Boolean,
+      default: false,
+      required: true
+    }
+  },
   data() {
     return {
-      showModal: false,
       selectedCategory: {}
     }
   },
@@ -55,36 +68,19 @@ export default {
     }),
     filteredByCategoryRecipes() {
       return this.openedRecipes.filter(openedRecipe => {
-        return openedRecipe.result.category === this.selectedCategory._id
+        return openedRecipe.result.category[0]._id === this.selectedCategory._id
       })
     }
   },
-  mounted() {
-    this.$root.$on('openedRecipesModalShow', () => {
-      this.showModal = true
-    })
-  },
   methods: {
-    selectCategory(category) {
-      this.selectedCategory = category
+    onClose() {
+      this.$emit('close')
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-@media screen and (min-width: map-get($grid-breakpoints, 'md')) {
-  .opened-recipes-categories {
-    max-height: 80vh;
-  }
-}
-
-@media screen and (max-width: map-get($grid-breakpoints, 'md')) {
-  .opened-recipes-categories {
-    max-height: 200px;
-  }
-}
-
 .recipes-row {
   min-height: 50vh;
 }
@@ -96,9 +92,9 @@ export default {
   opacity: 0.5;
   word-break: break-word;
   vertical-align: middle;
-  color: black;
-  background: rgb(240, 240, 240);
-  border: 1px solid map-get($colors, 'alchemy-light-green');
+  color: rgb(0, 0, 0);
+  background: rgb(245, 245, 245);
+  border: 1px solid map-get($colors, 'light-green');
   border-radius: 6px;
   transition: background-color 0.4s;
   width: 100%;
@@ -109,15 +105,20 @@ export default {
     font-weight: bold;
     opacity: 1;
     color: #ffffff;
-    background-color: map-get($colors, 'alchemy-green');
-    border: 1px solid map-get($colors, 'alchemy-light-green');
+    background-color: map-get($colors, 'green');
+    border: 1px solid map-get($colors, 'light-green');
   }
 }
 
 .opened-recipes-categories {
+  max-height: 200px;
   overflow-y: auto;
   direction: rtl;
   @extend %scrollbar;
+
+  @include media-md {
+    max-height: 80vh;
+  }
 }
 
 .recipes-list {
@@ -125,17 +126,5 @@ export default {
   min-height: 30vh;
 
   @extend %scrollbar;
-}
-
-.close-button {
-  background: none;
-  border: none;
-  outline: none;
-  color: black;
-  font-size: 1.3em;
-
-  &:hover {
-    opacity: 0.8;
-  }
 }
 </style>
