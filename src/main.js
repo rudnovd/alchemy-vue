865dc930404a79df4ff11284957e744178b82682
rudnovd <rudnov.d@gmail.com>
2020-01-13 00:38:26 +0500

    feat(app): import vue-tour

--- src/main.js
+++ src/main.js
@@ -6,13 +6,16 @@ import store from '@/store'
 
 import BootstrapVue from 'bootstrap-vue'
 import Vuelidate from 'vuelidate'
+import VueTour from 'vue-tour'
 
 import './css/main.scss'
+import 'vue-tour/dist/vue-tour.css'
 
 import '@/components/globals.js'
 
 Vue.use(BootstrapVue)
 Vue.use(Vuelidate)
+Vue.use(VueTour)
 
 Vue.config.productionTip = false
 
